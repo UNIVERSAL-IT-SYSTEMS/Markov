a2426774dd963d1741bf7927d6f95b9661bb22fd
Paul Liverman III <paul.liverman.iii@gmail.com>
2015-07-14 15:52:56 -0700

    wip updates

--- Markov.lua
+++ Markov.lua
@@ -6,6 +6,9 @@ local Markov = {}
 Markov.NO_WORD = "\n"
 Markov.chain_source = {}
 
+--setmetatable(Markov, {__call = function() return "something" end})
+-- then make each function of Markov check for a self argument?
+
 -- Returns iterator for processing text into individual words.
 function Markov.words(text)
     local position = 1
@@ -21,16 +24,20 @@ function Markov.words(text)
     end
 end
 
--- Adds a word that follows a key_word to chain_source.
-function Markov.add(key_word, word)
+-- Adds the next_word that follows a key_word to chain_source.
+function Markov.add(key_word, next_word)
+    --TODO if self then use self, else set self = Markov and use self ??
     if not Markov.chain_source[key_word] then
         Markov.chain_source[key_word] = {}
     end
-    insert(Markov.chain_source[key_word], word)
+    insert(Markov.chain_source[key_word], next_word)
 end
 
 -- Turns a source text into chain_source to generate random text from.
 function Markov.process_corpus(text)
+    --TODO make sure backslashes are escaped properly
+         --(they are only escaped on quotes right now)
+    --TODO multiple paragraphs (or just any \n's ?) need to have their starting key be \n AND the last word of whatever preceeded them
     Markov.chain_source = {}        --clear the old chain_source if needed
     local key = Markov.NO_WORD      --always starting from nothing
     for word in Markov.words(text) do
@@ -40,8 +47,12 @@ function Markov.process_corpus(text)
     Markov.add(key, Markov.NO_WORD) --always ending at nothing
 end
 
+--TODO function add_corpus(text)
+--      will add more text without erasing corpus
+
 -- Generate up to maxWords words of semi-sensible text!
 -- Assumes a source text has already been processed with Markov.process_corpus(text)
+-- TODO find out why this sometimes returns an empty string or " "
 function Markov.generate_text(maxWords)
     local key = Markov.NO_WORD      --always start at nothing
     local result = ""
@@ -49,7 +60,7 @@ function Markov.generate_text(maxWords)
         local array = Markov.chain_source[key]
         local word = array[random(#array)]
         if word == Markov.NO_WORD then
-            if result == " " then return Markov.generate_text(maxWords) end
+            if result == " " or result == "" then return Markov.generate_text(maxWords) end
             return result:sub(1,-2) --end if we have reached a NO_WORD
         end
         result = result .. word .. " "
@@ -66,5 +77,8 @@ function Markov.generate_from(text, maxWords)
     Markov.chain_source = tmp
     return result
 end
+
+-- dump_chain_source puts the chain_source into a file
+   -- (find a simple way of doing this without requiring a library)
 
 return Markov
